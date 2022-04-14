6fe53612e8253d1bf2688cfd328f020c43d99df3
Emmore <imohusoh@gmail.com>
2022-04-14 11:44:23 -0700

    task 4

---
+++ 0x00-hello_world/4-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+Char str[] " "\"programming is like building a multilingual puzzle";
+puts(str);
+return (0);
+}
